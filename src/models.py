f147fc5dbba6b6416d09be019ceedbe67705ac6e
elebar15 <197981071+elebar15@users.noreply.github.com>
2025-06-03 20:07:10 +0000

    Updated DB classes

--- src/models.py
+++ src/models.py
@@ -1,19 +1,63 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'User'
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(30), nullable=False)
+    firstname: Mapped[str] = mapped_column(String(100), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(Text, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    posts: Mapped[list["Post"]] = relationship("Post", back_populates="author")
+    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="author")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "is_active": self.is_active,
+            # do not serialize the password, it's a security breach
         }
+
+class Post(db.Model):
+    __tablename__ = 'POST'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('User.id'), nullable=False)
+
+    author: Mapped["User"] = relationship("User", back_populates="posts")
+    media: Mapped[list["Media"]] = relationship("Media", back_populates="post")
+    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="post")
+
+class Follower(db.Model):
+    __tablename__ = 'Follower'
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('User.id'), primary_key=True)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey('User.id'), primary_key=True)
+
+    user_from: Mapped["User"] = relationship("User", foreign_keys=[user_from_id], backref="followers_sent")
+    user_to: Mapped["User"] = relationship("User", foreign_keys=[user_to_id], backref="followers_received")
+
+class Media(db.Model):
+    __tablename__ = 'Media'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    media_type: Mapped[str] = mapped_column(String, nullable=False)
+    url: Mapped[str] = mapped_column(Text, nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('POST.id'), nullable=False)
+
+    post: Mapped["Post"] = relationship("Post", back_populates="media")
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    comment_text: Mapped[str] = mapped_column(Text, nullable=False)
+    author_id: Mapped[int] = mapped_column(ForeignKey('User.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('POST.id'), nullable=False)
+
+    author: Mapped["User"] = relationship("User", back_populates="comments")
+    post: Mapped["Post"] = relationship("Post", back_populates="comments")
